bb8196486428afbecf91a3fe0dac95fad2ce9096
LeeviPaa <leevi.paakkonen@hotmail.com>
2017-10-06 16:28:09 +0900

    Added master Includes unity basic project +sabre brushes for whiteboxing +VRTK for VR related tools +Steam VR for steamVR tools

---
+++ CastleRaid/Assets/VRTK/Scripts/Utilities/UnityEvents/VRTK_DashTeleport_UnityEvents.cs
@@ -0,0 +1,38 @@
+﻿namespace VRTK.UnityEventHelper
+{
+    using UnityEngine;
+    using UnityEngine.Events;
+    using System;
+
+    [AddComponentMenu("VRTK/Scripts/Utilities/Unity Events/VRTK_DashTeleport_UnityEvents")]
+    public sealed class VRTK_DashTeleport_UnityEvents : VRTK_UnityEvents<VRTK_DashTeleport>
+    {
+        [Serializable]
+        public sealed class DashTeleportEvent : UnityEvent<object, BlinkDashEventArgs> { }
+
+        public DashTeleportEvent OnWillDashThruObjects = new DashTeleportEvent();
+        public DashTeleportEvent OnDashedThruObjects = new DashTeleportEvent();
+
+        protected override void AddListeners(VRTK_DashTeleport component)
+        {
+            component.WillDashThruObjects += WillDashThruObjects;
+            component.DashedThruObjects += DashedThruObjects;
+        }
+
+        protected override void RemoveListeners(VRTK_DashTeleport component)
+        {
+            component.WillDashThruObjects -= WillDashThruObjects;
+            component.DashedThruObjects -= DashedThruObjects;
+        }
+
+        private void WillDashThruObjects(object o, BlinkDashEventArgs e)
+        {
+            OnWillDashThruObjects.Invoke(o, e);
+        }
+
+        private void DashedThruObjects(object o, BlinkDashEventArgs e)
+        {
+            OnDashedThruObjects.Invoke(o, e);
+        }
+    }
+}
