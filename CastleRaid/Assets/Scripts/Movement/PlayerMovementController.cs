25318fea6d6d5ba70098c855d7a38e938eee0abc
Mikko Juutinen <luzer59@hotmail.com>
2017-10-13 12:24:37 +0900

    Drag movement proto

---
+++ CastleRaid/Assets/Scripts/Movement/PlayerMovementController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public VRTK_PlayerClimb climbMovement;
+    public PlayerDragMovement dragMovement;
+    public VRTK_DashTeleport dashMovement;
+
+    void Awake()
+    {
+        if (!climbMovement)
+        {
+            climbMovement = GetComponent<VRTK_PlayerClimb>();
+        }
+        if (climbMovement)
+        {
+            climbMovement.PlayerClimbStarted += OnPlayerClimbStarted;
+            climbMovement.PlayerClimbEnded += OnPlayerClimbEnded;
+        }
+        if (!dragMovement)
+        {
+            dragMovement = GetComponent<PlayerDragMovement>();
+        }
+        if (!dashMovement)
+        {
+            dashMovement = GetComponent<VRTK_DashTeleport>();
+        }
+    }
+
+    void OnPlayerClimbStarted(object sender, PlayerClimbEventArgs e)
+    {
+        dragMovement.enabled = false;
+    }
+
+    void OnPlayerClimbEnded(object sender, PlayerClimbEventArgs e)
+    {
+        dragMovement.enabled = true;
+    }
+}
