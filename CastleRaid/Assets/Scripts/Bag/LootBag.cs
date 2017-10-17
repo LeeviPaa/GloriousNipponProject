e87d08df1b4cd188a16743429357e52a90969624
JuhoP <juzeliuz@suomi24.fi>
2017-10-17 14:43:43 +0900

    Untested LootBag created and added to the TestDoor scene. Created Lootable class to function with the LootBag and for lootable specific features (value, loot animation, etc.). Test asap on the VR pc.

---
+++ CastleRaid/Assets/Scripts/Bag/LootBag.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootBag : MonoBehaviour
+{
+    //private AudioSource sound;
+    //private UnityEngine.UI.Text txtBelongings;
+
+    [SerializeField]
+    private Collider lootAreaTrigger;
+    [SerializeField]
+    private GameObject bagMesh;
+    [SerializeField]
+    Transform lootDestinationTransform;
+    List<Lootable> lootablesInLootTrigger = new List<Lootable>();
+    int lootCount = -1;
+    int lootTotalValue = -1;
+    bool isActive = false;
+
+    public delegate void IntVoid(int integer);
+    public event IntVoid OnLootCountChange;
+    public event IntVoid OnLootTotalValueChange;
+
+    private void Start()
+    {
+        SetActiveState(true);
+
+        lootCount = 0;
+        lootTotalValue = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isActive)
+        {
+            //If there are any items in the list of lootables inside the looting trigger
+            int count = lootablesInLootTrigger.Count;
+            if (count > 0)
+            {
+                //Loop through all of them one by one
+                for (int i = 0; i < count; i++)
+                {
+                    //If the object is not currently grabbed
+                    if (!lootablesInLootTrigger[i].GetIsGrabbed())
+                    {
+                        //Loot it and remove it from the list
+                        lootablesInLootTrigger[i].OnLootingFinished += OnLootableLooted;
+                        lootablesInLootTrigger[i].Loot(lootDestinationTransform);
+                        lootablesInLootTrigger.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+
+    public void SetParentTransform(Transform newParent)
+    {
+        //Set a new transform for the loot bag to follow
+        transform.SetParent(newParent);
+    }
+
+    public void SetActiveState(bool newState)
+    {
+        //Set whether the bag is active or not
+        isActive = newState;
+
+        if (isActive)
+        {
+            lootAreaTrigger.enabled = true;
+
+            if(bagMesh != null)
+            {
+                bagMesh.SetActive(true);
+            }
+        }
+        else
+        {
+            lootAreaTrigger.enabled = false;
+
+            if (bagMesh != null)
+            {
+                bagMesh.SetActive(false);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("LootBag.OnTriggerEnter");
+
+        if (isActive)
+        {
+            //If the entering object is a lootable
+            Lootable lootable = other.GetComponent<Lootable>();
+            if (lootable)
+            {
+                Debug.Log("LootBag.OnTriggerEnter, lootable component found on entering object");
+                //If it is not currently grabbed
+                if (!lootable.GetIsGrabbed())
+                {
+                    //Loot it
+                    lootable.OnLootingFinished += OnLootableLooted;
+                    lootable.Loot(lootDestinationTransform);
+                }
+                else
+                {
+                    //Else, add it to the list of lootables inside the trigger
+                    lootablesInLootTrigger.Add(lootable);
+                }
+            }
+        }
+
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (isActive)
+        {
+            Lootable lootable = other.GetComponent<Lootable>();
+            //If the exiting object is a lootable
+            if (lootable)
+            {
+                //If it is contained in the list of lootables inside the trigger
+                if (lootablesInLootTrigger.Contains(lootable))
+                {
+                    //Remove it from the list
+                    lootablesInLootTrigger.Remove(lootable);
+                }
+            }
+        }
+
+    }
+
+    private void OnLootableLooted(int lootValue)
+    {
+        lootCount++;
+        if(OnLootCountChange != null)
+        {
+            OnLootCountChange(lootCount);
+        }
+
+        lootTotalValue += lootValue;
+        if (OnLootTotalValueChange != null)
+        {
+            OnLootTotalValueChange(lootTotalValue);
+        }
+
+        //Call appropriate visual / sound effects here if the effects are the same regardless of the lootable
+    }
+
+    public bool GetActiveState()
+    {
+        return isActive;
+    }
+
+    public int GetLootCount()
+    {
+        return lootCount;
+    }
+
+    public int GetLootTotalValue()
+    {
+        return lootTotalValue;
+    }
+
+}
---
+++ CastleRaid/Assets/Scripts/Bag/Lootable.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Lootable : MonoBehaviour
+{
+    VRTK_InteractableObject interactableObjectScript;
+    bool isGrabbed = false;
+    bool isLooted = false;
+    [SerializeField]
+    int lootValue = 100;
+
+    public delegate void IntVoid(int integer);
+    public event IntVoid OnLootingFinished;
+
+    bool minimizingToBag = false;
+    Vector3 minimizingStartScale = Vector3.zero;
+    Vector3 minimizingEndScale = Vector3.zero;
+    Vector3 minimizingStartPos = Vector3.zero;
+    float minimizingStartTime = -1f;
+    float minimizingDuration = 0.5f;
+
+    private void Start()
+    {
+        minimizingToBag = false;
+    }
+
+    void OnEnable()
+    {
+        interactableObjectScript = GetComponent<VRTK_InteractableObject>();
+
+        if (interactableObjectScript)
+        {
+            interactableObjectScript.InteractableObjectGrabbed += OnStartGrab;
+            interactableObjectScript.InteractableObjectUngrabbed += OnEndGrab;
+        }
+    }
+    void OnDisable()
+    {
+        if (interactableObjectScript)
+        {
+            interactableObjectScript.InteractableObjectGrabbed -= OnStartGrab;
+            interactableObjectScript.InteractableObjectUngrabbed -= OnEndGrab;
+        }
+
+        if (minimizingToBag)
+        {
+            FinishLooting();
+        }
+    }
+
+    void OnStartGrab(object sender, InteractableObjectEventArgs e)
+    {
+        isGrabbed = true;
+        print("OnStartGrab");
+
+        //Change color: red
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer)
+        {
+            GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0);
+        }
+    }
+
+    void OnEndGrab(object sender, InteractableObjectEventArgs e)
+    {
+        print("OnEndGrab");
+
+        //Change color: blue
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer)
+        {
+            meshRenderer.material.color = new Color(0, 0, 1);
+        }
+        isGrabbed = false;
+    }
+
+    public bool GetIsGrabbed()
+    {
+        return isGrabbed;
+    }
+
+    public int GetLootValue()
+    {
+        return lootValue;
+    }
+
+    public void Loot(Transform lootingPoint)
+    {
+        //Set the lootable state to is looted
+        isLooted = true;
+
+        //Start the loot "animation"
+        minimizingToBag = true;
+        transform.SetParent(lootingPoint);
+        minimizingStartPos = transform.localPosition;
+        minimizingStartScale = transform.localScale;
+        minimizingStartTime = Time.time;
+    }
+
+    private void MinimizeToLootBag()
+    {
+        float timeSinceStarted = Time.time - minimizingStartTime;
+        float percentageCompleted = timeSinceStarted / minimizingDuration;
+
+        transform.localScale = Vector3.Lerp(minimizingStartScale, minimizingEndScale, percentageCompleted);
+        transform.localPosition = Vector3.Lerp(minimizingStartPos, Vector3.zero, percentageCompleted);
+
+        if(percentageCompleted >= 1)
+        {
+            minimizingToBag = false;
+            FinishLooting();
+        }
+    }
+
+    private void FinishLooting()
+    {
+        if (OnLootingFinished != null)
+        {
+            OnLootingFinished(lootValue);
+        }
+
+        OnLootingFinished = null;
+        //Call the visual / sound effects here, if unique to each lootable
+
+        gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+        if (minimizingToBag)
+        {
+            MinimizeToLootBag();
+        }
+    }
+
+}
