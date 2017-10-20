1e247b49147aaeb9e17fb07c174dfae8c225c803
Mikko Juutinen <luzer59@hotmail.com>
2017-10-20 15:23:45 +0900

    Modified drag mechanic utilising VRTK_InteractGrab events

--- CastleRaid/Assets/Scripts/Movement/PlayerDragMovement.cs
+++ CastleRaid/Assets/Scripts/Movement/PlayerDragMovement.cs
@@ -9,8 +9,8 @@ public class PlayerDragMovement : MonoBehaviour
     public VRTK_BodyPhysics bodyPhysics;
     public float dragSpeed = 1f;
 
-    private enum State { Active, Inactive, Locked }
-	private State dragState = State.Inactive;
+    private enum State { WaitingForInput, WaitingForConfirmation, Active, Locked }
+	private State dragState = State.WaitingForInput;
     private Transform playArea;
     private Vector3 controllerDragStartLocalPos = Vector3.zero;
     private Vector3 playAreaDragStartPos = Vector3.zero;
@@ -41,8 +41,10 @@ public class PlayerDragMovement : MonoBehaviour
     {
         for (int i = 0; i < grab.Length; i++)
         {
-            grab[i].ControllerGrabInteractableObject += OnPlayerGrab;
-            grab[i].ControllerUngrabInteractableObject += OnPlayerUngrab;
+            grab[i].ControllerGrabInteractableObject += OnPlayerGrabObject;
+            grab[i].ControllerUngrabInteractableObject += OnPlayerUngrabObject;
+            grab[i].GrabButtonPressed += OnPlayerGrabButtonPressed;
+            grab[i].GrabButtonReleased += OnPlayerGrabButtonReleased;
         }
     }
 
@@ -50,99 +52,71 @@ public class PlayerDragMovement : MonoBehaviour
     {
         for (int i = 0; i < grab.Length; i++)
         {
-            grab[i].ControllerGrabInteractableObject -= OnPlayerGrab;
-            grab[i].ControllerUngrabInteractableObject -= OnPlayerUngrab;
+            grab[i].ControllerGrabInteractableObject -= OnPlayerGrabObject;
+            grab[i].ControllerUngrabInteractableObject -= OnPlayerUngrabObject;
+            grab[i].GrabButtonPressed -= OnPlayerGrabButtonPressed;
+            grab[i].GrabButtonReleased -= OnPlayerGrabButtonReleased;
         }
-    }
-
-    void Start()
-    {
-        StartCoroutine(DelayedStart());
-    }
-
-    IEnumerator DelayedStart()
-    {
-        yield return null;
-
-        playArea = VRTK_DeviceFinder.PlayAreaTransform();
-
-
-        InitControllerListeners(VRTK_DeviceFinder.GetControllerLeftHand(), true);
-        InitControllerListeners(VRTK_DeviceFinder.GetControllerRightHand(), true);
-    }
-
-    void InitControllerListeners(GameObject controller, bool state)
-    {
-        if (controller)
-        {
-            VRTK_ControllerEvents controllerEvents = controller.GetComponent<VRTK_ControllerEvents>();
-            if (state)
-            {
-                controllerEvents.GripPressed += GripPressed;
-                controllerEvents.GripReleased += GripReleased;
-            }
-            else
-            {
-                controllerEvents.GripPressed -= GripPressed;
-                controllerEvents.GripReleased -= GripReleased;
-            }
-        }
-    }
-
-    void GripPressed(object sender, ControllerInteractionEventArgs e)
-    {
-		if (dragState == State.Locked)
-		{
-			return;
-		}
-		GameObject controller = VRTK_DeviceFinder.GetActualController(((VRTK_ControllerEvents)sender).gameObject);
-		if (bodyPhysics.OnGround()) 
-		{
-			currentUsedController = controller;
-			controllerDragStartLocalPos = currentUsedController.transform.localPosition;
-			playAreaDragStartPos = playArea.position;
-			//bodyPhysics.ResetFalling();
-			//bodyPhysics.TogglePreventSnapToFloor(true);
-			//bodyPhysics.enableBodyCollisions = false;
-			//bodyPhysics.ToggleOnGround(false);
-			dragState = State.Active;
-		}
-	}
-
-    void GripReleased(object sender, ControllerInteractionEventArgs e)
-    {
-        GameObject controller = VRTK_DeviceFinder.GetActualController(((VRTK_ControllerEvents)sender).gameObject);
-		if (controller == currentUsedController)
-        {
-			currentUsedController = null;
-			//bodyPhysics.TogglePreventSnapToFloor(false);
-			//bodyPhysics.enableBodyCollisions = true;
-			//bodyPhysics.ToggleOnGround(true);
-			if (dragState == State.Active)
-			{
-				dragState = State.Inactive;
-			}
-		}
     }
 
     void Update()
     {
+        if (dragState == State.WaitingForConfirmation)
+        {
+            BeginDrag();
+        }
+           
         if (dragState == State.Active && playArea)
         {
-			Vector3 finalPos = playAreaDragStartPos + ((controllerDragStartLocalPos - currentUsedController.transform.localPosition) * dragSpeed);
-			finalPos.y = playArea.position.y;
-			playArea.position = finalPos;
-            bodyPhysics.ForceSnapToFloor();
+            Vector3 finalPos = playAreaDragStartPos + ((controllerDragStartLocalPos - currentUsedController.transform.localPosition) * dragSpeed);
+            finalPos.y = playArea.position.y;
+            playArea.position = finalPos;
         }
-	}
+    }
 
-	void OnPlayerGrab(object sender, ObjectInteractEventArgs e)
-	{
-		dragState = State.Locked;
-	}
-
-    void OnPlayerUngrab(object sender, ObjectInteractEventArgs e)
+    void BeginDrag()
     {
-		dragState = State.Inactive;
+        dragState = State.Active;
+		controllerDragStartLocalPos = currentUsedController.transform.localPosition;
+		playAreaDragStartPos = playArea.position;
 	}
+
+    void EndDrag()
+    {
+        dragState = State.WaitingForInput;
+        currentUsedController = null;
+    }
+
+    void OnPlayerGrabButtonPressed(object sender, ControllerInteractionEventArgs e)
+    {
+        if (dragState == State.WaitingForInput)
+        {
+            currentUsedController = e.controllerReference.actual;
+            dragState = State.WaitingForConfirmation;
+        }
+    }
+
+    void OnPlayerGrabButtonReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        if (dragState == State.Active && currentUsedController == e.controllerReference.actual)
+        {
+            EndDrag();
+        }
+    }
+
+    void OnPlayerGrabObject(object sender, ObjectInteractEventArgs e)
+	{
+        if (currentUsedController == e.controllerReference.actual)
+        {
+            dragState = State.Locked;
+        }
+    }
+
+    void OnPlayerUngrabObject(object sender, ObjectInteractEventArgs e)
+    {
+        if (currentUsedController == e.controllerReference.actual)
+        {
+            dragState = State.WaitingForInput;
+        }
+    }
 }
