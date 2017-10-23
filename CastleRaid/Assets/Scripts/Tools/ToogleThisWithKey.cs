e028e16a0046d301d0b30a18c061c6f82dec997e
LeeviPaa <leevi.paakkonen@hotmail.com>
2017-10-23 15:49:18 +0900

    First edition of the map whiteblocked

---
+++ CastleRaid/Assets/Scripts/Tools/ToogleThisWithKey.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct ObjectKeyPair
+{
+    public GameObject toggleObject;
+    public KeyCode toggleKey;
+    private bool onOff;
+    public bool Toggle()
+    {
+        onOff = !onOff;
+        return onOff;
+    }
+}
+
+//[ExecuteInEditMode]
+public class ToogleThisWithKey : MonoBehaviour {
+
+    public List<ObjectKeyPair> L_ObjectKeyPair;
+
+    private void OnSceneGUI()
+    {
+        foreach(ObjectKeyPair OKP in L_ObjectKeyPair)
+        {
+            if (OKP.toggleObject != null && OKP.toggleKey != null)
+            {
+                Event e = Event.current;
+                switch (Event.current.type)
+                {
+                    case EventType.KeyDown:
+                        {
+                            if (Event.current.keyCode == OKP.toggleKey)
+                            {
+                                OKP.toggleObject.SetActive(OKP.Toggle());
+                            }
+                            break;
+                        }
+                    default:
+                        {
+                            break;
+                        }
+            }
+            }
+        }
+    }
+
+}
