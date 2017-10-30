90d86651c7176fefb00de0f43b2c9abdc6a05451
MasujimaRyohei <nushibeatbox@gmail.com>
2017-10-30 16:08:38 +0900

    Sub_VRTK_InteractTouch.cs can indicate whether it is climbable or not.

--- CastleRaid/Assets/Scripts/Effects/Sub_VRTK_InteractTouch.cs
+++ CastleRaid/Assets/Scripts/Effects/Sub_VRTK_InteractTouch.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
+﻿
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
 public class Sub_VRTK_InteractTouch : VRTK_InteractTouch {
 
-    protected new void OnTriggerStay(Collider collider)
+    [SerializeField]
+    [ColorUsage(false,true,0f,10f,0.125f,3f)]
+    private Color climbableIndicateColor;
+
+    protected override void OnTriggerStay(Collider collider)
     {
         GameObject colliderInteractableObject = TriggerStart(collider);
 
@@ -28,16 +34,53 @@ public class Sub_VRTK_InteractTouch : VRTK_InteractTouch {
             OnControllerStartTouchInteractableObject(SetControllerInteractEvent(touchedObject));
             StoreTouchedObjectColliders(collider);
 
-            //touchedObjectScript.ToggleHighlight(true);
-            var color = new Color(0, 0, 0);
-            var mat = touchedObject.GetComponent<Material>();
-            mat.EnableKeyword("_EMISSION");
-            mat.SetColor("_EmissionColor", color);
+            if (touchedObject.GetComponent<VRTK.GrabAttachMechanics.VRTK_ClimbableGrabAttach>())
+            {
+                var color = climbableIndicateColor;
+                var mat = touchedObject.GetComponent<Renderer>();
+                mat.material.EnableKeyword("_EMISSION");
+                mat.material.SetColor("_EmissionColor", color);
+            }
+            else
+            {
+                touchedObjectScript.ToggleHighlight(true);
+            }
             ToggleControllerVisibility(false);
             CheckRumbleController(touchedObjectScript);
             touchedObjectScript.StartTouching(this);
 
             OnControllerTouchInteractableObject(SetControllerInteractEvent(touchedObject));
         }
+
+    }
+
+    protected override void StopTouching(GameObject untouched)
+    {
+        OnControllerStartUntouchInteractableObject(SetControllerInteractEvent(untouched));
+        if (IsObjectInteractable(untouched))
+        {
+            VRTK_InteractableObject untouchedObjectScript = (untouched != null ? untouched.GetComponent<VRTK_InteractableObject>() : null);
+            if (untouchedObjectScript != null)
+            {
+                untouchedObjectScript.StopTouching(this);
+                if (!untouchedObjectScript.IsTouched())
+                {
+                    if (untouched.GetComponent<VRTK.GrabAttachMechanics.VRTK_ClimbableGrabAttach>())
+                    {
+                        var color = climbableIndicateColor;
+                        var mat = untouched.GetComponent<Renderer>();
+                        mat.material.DisableKeyword("_EMISSION");
+                    }
+                    else
+                    {
+                        untouchedObjectScript.ToggleHighlight(false);
+                    }
+                }
+            }
+        }
+
+        ToggleControllerVisibility(true);
+        OnControllerUntouchInteractableObject(SetControllerInteractEvent(untouched));
+        CleanupEndTouch();
     }
 }
