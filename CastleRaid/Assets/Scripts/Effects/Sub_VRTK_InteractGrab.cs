ad8b5ff982ac557ef281f026662969db8af633e0
MasujimaRyohei <nushibeatbox@gmail.com>
2017-10-31 11:46:17 +0900

    Update

--- CastleRaid/Assets/Scripts/Effects/Sub_VRTK_InteractGrab.cs
+++ CastleRaid/Assets/Scripts/Effects/Sub_VRTK_InteractGrab.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 using VRTK;
 public class Sub_VRTK_InteractGrab : VRTK_InteractGrab
 {
-    void Start()
-    {
-        var device0 = SteamVR_Controller.Input(0);
-        device0.TriggerHapticPulse(2000);
-        var device1 = SteamVR_Controller.Input(1);
-        device1.TriggerHapticPulse(2000);
-    }
     protected override void OnEnable()
     {
         base.OnEnable();
@@ -24,6 +17,7 @@ public class Sub_VRTK_InteractGrab : VRTK_InteractGrab
     protected override void OnDisable()
     {
         base.OnDisable();
+
         var e = GetComponent<VRTK_ControllerEvents>();
         if (e)
             e.GripPressed -= OnGrabbed;
@@ -31,13 +25,16 @@ public class Sub_VRTK_InteractGrab : VRTK_InteractGrab
     }
     private void OnGrabbed(object sender, ControllerInteractionEventArgs e)
     {
-        if (grabbedObject)
-            if (grabbedObject.GetComponent<VRTK.GrabAttachMechanics.VRTK_ClimbableGrabAttach>())
-            {
-                SDK_BaseController.ControllerHand hand = VRTK_DeviceFinder.GetControllerHand(gameObject);
-                var device = SteamVR_Controller.Input((int)hand);
-                device.TriggerHapticPulse(2000);
-                print(device.ToString()+hand.ToString() + " Viveration!!");
-            }
+        if (!grabbedObject && 
+            !grabbedObject.GetComponent<VRTK.GrabAttachMechanics.VRTK_ClimbableGrabAttach>())
+            return;
+
+        var a = transform.parent.GetComponent<SteamVR_TrackedObject>();
+
+        if (!a) return;
+
+        var device = SteamVR_Controller.Input((int)a.index);
+        device.TriggerHapticPulse(2000);
+        print(device.ToString() + " Viveration!!");
     }
 }
