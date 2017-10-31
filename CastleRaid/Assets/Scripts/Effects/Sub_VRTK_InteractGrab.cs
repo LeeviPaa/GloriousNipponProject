0f0805e0b12e317636f8987a68849929e3ccfc1e
MasujimaRyohei <nushibeatbox@gmail.com>
2017-10-31 10:21:29 +0900

    Add Sub_VRTK_InteractGrab.cs

---
+++ CastleRaid/Assets/Scripts/Effects/Sub_VRTK_InteractGrab.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+public class Sub_VRTK_InteractGrab : VRTK_InteractGrab
+{
+    void Start()
+    {
+
+    }
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+
+        GetComponent<VRTK_ControllerEvents>().GripPressed += OnGrabbed;
+        
+    }
+
+    protected override void OnDisable()
+    {
+        base.OnDisable();
+        GetComponent<VRTK_ControllerEvents>().GripPressed -= OnGrabbed;
+
+    }
+    private void OnGrabbed(object sender, ControllerInteractionEventArgs e)
+    {
+        print("Grabbed");
+    }
+}
