456cb240b886356c7f7578dd2a59265205d79f04
MasujimaRyohei <nushibeatbox@gmail.com>
2017-11-01 16:07:19 +0900

    Fixed a bug when Using vrtk simulator

--- CastleRaid/Assets/Scripts/Effects/ClimbingIndicator.cs
+++ CastleRaid/Assets/Scripts/Effects/ClimbingIndicator.cs
@@ -6,36 +6,37 @@ using VRTK;
 public class ClimbingIndicator : MonoBehaviour
 {
     [SerializeField]
-    GameObject[] hands;
+    VRTK_InteractGrab[] grabs;
 
     bool enableViveration = true;
     [SerializeField]
     AudioClip[] grabSound;
     AudioSource audioSource;
 
+    ControllerInteractionEventArgs? temp = null;
 
     void Start()
     {
         audioSource = gameObject.AddComponent<AudioSource>();
         audioSource.playOnAwake = false;
-        if (!GetHand(SDK_BaseController.ControllerHand.Left)||!GetHand( SDK_BaseController.ControllerHand.Right) )
-        {
-            Debug.LogError("Missing attachment!");
-        }
+        //if (!GetHand(SDK_BaseController.ControllerHand.Left)||!GetHand( SDK_BaseController.ControllerHand.Right) )
+        //{
+        //    Debug.LogError("Missing attachment!");
+        //}
 
-        var leftEvents = GetHand(SDK_BaseController.ControllerHand.Left).GetComponent<VRTK_ControllerEvents>();
-        leftEvents.GripPressed += OnGrabbed;
+        //var leftEvents = GetHand(SDK_BaseController.ControllerHand.Left).GetComponent<VRTK_ControllerEvents>();
+        //leftEvents.GripPressed += OnGrabbed;
 
-        var rightEvents = GetHand(SDK_BaseController.ControllerHand.Right).GetComponent<VRTK_ControllerEvents>();
-        rightEvents.GripPressed += OnGrabbed;
+        //var rightEvents = GetHand(SDK_BaseController.ControllerHand.Right).GetComponent<VRTK_ControllerEvents>();
+        //rightEvents.GripPressed +=OnGrabbed;
 
 
 
     }
-    GameObject GetHand(SDK_BaseController.ControllerHand hand)
-    {
-        return hands[(int)hand - 1];
-    }
+    //GameObject GetHand(SDK_BaseController.ControllerHand hand)
+    //{
+    //    return hands[(int)hand - 1];
+    //}
     public void SetActiveControllerViveration(bool value)
     {
         enableViveration = value;
@@ -43,7 +44,8 @@ public class ClimbingIndicator : MonoBehaviour
     protected void OnEnable()
     {
         // controllerRightHandEvents.GripPressed += OnGrabbed;
-
+        foreach (var g in grabs)
+            g.ControllerGrabInteractableObject += OnGrabbed;
     }
 
     protected void OnDisable()
@@ -51,20 +53,26 @@ public class ClimbingIndicator : MonoBehaviour
         //  controllerRightHandEvents.GripPressed -= OnGrabbed;
 
     }
-    private void OnGrabbed(object sender, ControllerInteractionEventArgs e)
+    private void OnGrabbed(object sender, ObjectInteractEventArgs e)
     {
         print(e.controllerReference.hand + "Grabbed!");
-
-        var hand = GetHand(e.controllerReference.hand);
+        var hand = e.controllerReference.scriptAlias;
         var handGrab = hand.GetComponent<VRTK_InteractGrab>();
         if (!handGrab.GetGrabbedObject())
+        {
+            print(handGrab.GetGrabbedObject());
             return;
+        }
         if (!handGrab.GetGrabbedObject().GetComponent<VRTK.GrabAttachMechanics.VRTK_ClimbableGrabAttach>())
             return;
 
-        var tracedObject = hand.transform.parent.GetComponent<SteamVR_TrackedObject>();
+        var tracedObject = e.controllerReference.actual.GetComponent<SteamVR_TrackedObject>();
 
-        if (!tracedObject) return;
+        if (!tracedObject)
+        {
+            print("Not steamVR " + e.controllerReference.actual);
+            return;
+        }
 
         if (enableViveration)
         {
@@ -74,5 +82,14 @@ public class ClimbingIndicator : MonoBehaviour
         }
 
         audioSource.PlayOneShot(grabSound[Random.Range(0, grabSound.Length)]);
+    }
+   
+    void Update()
+    {
+        if(temp != null)
+        if(temp.Value.controllerReference.actual)
+        {
+            print(temp.Value.controllerReference.scriptAlias.GetComponent<VRTK_InteractGrab>().GetGrabbedObject());
+        }
     }
 }
