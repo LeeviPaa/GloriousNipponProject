470fa9b6278cafbbdf47954104853b6ffb6f793c
Mikko Juutinen <luzer59@hotmail.com>
2017-11-02 16:18:44 +0900

    Made PlayerRelocator and added it to player, made a base for HookShot and rearranged some scripts.

---
+++ CastleRaid/Assets/Scripts/Gadgets/Gadget_HookShot.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class Gadget_HookShot : MonoBehaviour
+{
+    public VRTK_InteractableObject interaction;
+
+    void Start()
+    {
+        if (!interaction)
+        {
+            interaction = GetComponent<VRTK_InteractableObject>();
+        }
+
+        if (interaction)
+        {
+            interaction.InteractableObjectUsed += OnUseBegin;
+            interaction.InteractableObjectUnused += OnUseEnd;
+            interaction.InteractableObjectGrabbed += OnGrabBegin;
+            interaction.InteractableObjectUngrabbed += OnGrabEnd;
+        }
+        else
+        {
+            enabled = false;
+        }
+    }
+
+    void OnGrabBegin(object sender, InteractableObjectEventArgs e)
+    {
+
+    }
+
+    void OnGrabEnd(object sender, InteractableObjectEventArgs e)
+    {
+        
+    }
+
+    void OnUseBegin(object sender, InteractableObjectEventArgs e)
+    {
+        PlayerRelocator relocator = GameManager.levelInstance.GetPlayerScriptHolder().GetComponent<PlayerRelocator>();
+        if (relocator)
+        {
+
+        }
+    }
+
+    void OnUseEnd(object sender, InteractableObjectEventArgs e)
+    {
+
+    }
+}
