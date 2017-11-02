f4054c3ca7065e89d9bea64575780fdadd5bd2c8
MasujimaRyohei <nushibeatbox@gmail.com>
2017-11-02 16:58:12 +0900

    Add virtual keyboard scripts and prefab.

---
+++ CastleRaid/Assets/Scripts/UI/VirtualKeyboard/Keytop.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Keytop : MonoBehaviour
+{
+    public void PushKey()
+    {
+        print(transform.GetChild(0).GetComponent<Text>().text);
+    }
+}
