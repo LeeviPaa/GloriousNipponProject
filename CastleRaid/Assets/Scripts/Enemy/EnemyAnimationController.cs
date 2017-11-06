4a3bbd9026f1c25196a3a94331bc50a6842aef37
JuhoP <juzeliuz@suomi24.fi>
2017-11-06 15:12:06 +0900

    Implemented attack detection and ragdolling to the enemyPatroller.

---
+++ CastleRaid/Assets/Scripts/Enemy/EnemyAnimationController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class EnemyAnimationController : MonoBehaviour
+{
+    #region References & variables
+    Animator animator;
+    bool isActive = false;
+    #endregion
+
+    #region Initialization
+    private void Start()
+    {
+        FindAnimatorReference();
+    }
+
+    private void FindAnimatorReference()
+    {
+        if (animator == null)
+        {
+            animator = GetComponent<Animator>();
+        }
+    }
+    #endregion
+
+    #region Activation
+    public void SetActiveState(bool newState)
+    {
+        FindAnimatorReference();
+
+        if (animator != null)
+        {
+            animator.enabled = newState;
+        }
+
+        isActive = newState;
+    }
+    #endregion
+
+    #region Animation calls
+    public void PlayAttackAnimation()
+    {
+        if (isActive)
+        {
+            animator.Play("");
+        }
+    }
+
+    public void PlayGuardAnimation()
+    {
+        if (isActive)
+        {
+            animator.Play("");
+        }
+    }
+
+    public void PlayCheckPerimeterAnimation()
+    {
+        if (isActive)
+        {
+            animator.Play("");
+        }
+    }
+    #endregion
+
+    #region Animation states
+    public void SetMovementSpeed(float newSpeed)
+    {
+        if (isActive)
+        {
+            animator.SetFloat("MovementSpeed", newSpeed);
+        }
+    }
+    #endregion
+
+}
