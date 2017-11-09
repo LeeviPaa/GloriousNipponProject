74625e85bcd46734339846fc8e71493600ee75f8
Mikko Juutinen <luzer59@hotmail.com>
2017-11-09 10:44:27 +0900

    Fixed wrong rotation on drag movement after rotating play area, added fade for player relocation and made a script for playing audio on object collisions with threshold velocity and repeat limit.

---
+++ CastleRaid/Assets/Scripts/Audio/ObjectCollisionAudio.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCollisionAudio : MonoBehaviour
+{
+    [SerializeField]
+    private string audioName;
+    [SerializeField]
+    private float audioBlockTime;
+    [SerializeField]
+    private float minimumReqVelocity;
+
+    private bool audioPlayable = true;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (audioPlayable && col.relativeVelocity.magnitude >= minimumReqVelocity)
+        {
+            GameManager.audioManager.GetAudio(audioName, true, true, col.contacts[0].point, transform);
+            audioPlayable = false;
+            StartCoroutine(ClearAudioPlayBlock());
+        }       
+    }
+
+    IEnumerator ClearAudioPlayBlock()
+    {
+        yield return new WaitForSeconds(audioBlockTime);
+        audioPlayable = true;
+    }
+}
