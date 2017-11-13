999ccfc4b24a91ff2447f986fd3dc8aa86f4c6b9
VRPC <eiooemailia@perunasalaatti.com>
2017-11-13 15:18:44 +0900

    Tested and tweaked experimental gyrocopter gadget. Fixed tower crucifix transform streching glitch.

--- CastleRaid/Assets/Scripts/Gadgets/Gadget_Gyrocopter.cs
+++ CastleRaid/Assets/Scripts/Gadgets/Gadget_Gyrocopter.cs
@@ -3,70 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 using VRTK;
 
-public class Gadget_Gyrocopter : MonoBehaviour
-{
-    [SerializeField]
-    Transform propellerTransform;
-    VRTK_InteractableObject interactableScript;
-    Rigidbody rb;
-    bool isUsed = false;
-    float liftForce = 800f;
-    float rotateSpeed = 1080f;
+public class Gadget_Gyrocopter : MonoBehaviour {
+	[SerializeField]
+	Transform propellerTransform;
+	VRTK_InteractableObject interactableScript;
+	Rigidbody copterRigidbody;
+	Rigidbody grabberRigidbody;
+	bool isUsed = false;
+	[SerializeField]
+	float liftForce = 800f;
+	[SerializeField]
+	float cameraRigLiftForceMultiplier = 10f;
+	[SerializeField]
+	float flyVelocityMagnitudeLimit = 5f;
+	float rotateSpeed = 1080f;
 
-    private void Awake()
-    {
-        interactableScript = GetComponent<VRTK_InteractableObject>();
-        interactableScript.InteractableObjectUsed += OnUse;
-        interactableScript.InteractableObjectUnused += OnUnuse;
+	private void Awake() {
+		interactableScript = GetComponent<VRTK_InteractableObject>();
+		interactableScript.InteractableObjectUsed += OnUse;
+		interactableScript.InteractableObjectUnused += OnUnuse;
 
-        rb = GetComponent<Rigidbody>();
-    }
+		copterRigidbody = GetComponent<Rigidbody>();
+	}
 
-    private void Update()
-    {
-        if (isUsed)
-        {
-            Vector3 newPropellerRotation = Vector3.zero;
-            newPropellerRotation.y = rotateSpeed * Time.deltaTime;
-            propellerTransform.rotation *= Quaternion.Euler(newPropellerRotation);
-        }
+	private void Update() {
+		if (isUsed) {
+			Vector3 newPropellerRotation = Vector3.zero;
+			newPropellerRotation.y = rotateSpeed * Time.deltaTime;
+			propellerTransform.rotation *= Quaternion.Euler(newPropellerRotation);
+		}
+	}
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            //if (isUsed)
-            //{
-            //    OnUnuse(this, new InteractableObjectEventArgs());
-            //}
-            //else
-            //{
-            //    OnUse(this, new InteractableObjectEventArgs());
-            //}
+	private void FixedUpdate() {
+		if (isUsed) {
+			if (grabberRigidbody != null) {
+				grabberRigidbody.isKinematic = false;
+				grabberRigidbody.AddForce(transform.up * liftForce * cameraRigLiftForceMultiplier * Time.fixedDeltaTime);
 
-            OnUse(this, new InteractableObjectEventArgs());
-        }
+				//LimitVelocity(grabberRigidbody, flyVelocityMagnitudeLimit);
+			} else {
+				copterRigidbody.AddForce(transform.up * liftForce * Time.fixedDeltaTime);
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            OnUnuse(this, new InteractableObjectEventArgs());
-        }
-    }
+				//LimitVelocity(copterRigidbody, flyVelocityMagnitudeLimit);
+			}
+		}
+	}
 
-    private void FixedUpdate()
-    {
-        if (isUsed)
-        {
-            rb.AddForce(transform.up * liftForce * Time.fixedDeltaTime);
-        }
-    }
+	private void LimitVelocity(Rigidbody rb, float velMagLimit)
+	{
+		Debug.Log("rb.velocity.magnitude: " + rb.velocity.magnitude);
+		if(rb.velocity.magnitude > velMagLimit)
+		{
+			rb.velocity = rb.velocity.normalized * velMagLimit;
+			Debug.Log("rb.velocity.magnitude after limiting: " + rb.velocity.magnitude);
+		}
+	}
 
-    private void OnUse(object sender, InteractableObjectEventArgs e)
-    {
-        isUsed = true;
-    }
+	private void OnUse(object sender, InteractableObjectEventArgs e) {
 
-    private void OnUnuse(object sender, InteractableObjectEventArgs e)
-    {
-        isUsed = false;
-    }
+		VRTK_PlayerObject playerObjectScript = e.interactingObject.GetComponentInParent<VRTK_PlayerObject>();
+		if (playerObjectScript != null) {
+
+			if (playerObjectScript.objectType == VRTK_PlayerObject.ObjectTypes.CameraRig) {
+				grabberRigidbody = playerObjectScript.GetComponent<Rigidbody>();
+
+			} else if (grabberRigidbody == null) {
+				playerObjectScript = playerObjectScript.transform.parent.GetComponentInParent<VRTK_PlayerObject>();
+
+				if (playerObjectScript != null) {
+
+					if (playerObjectScript.objectType == VRTK_PlayerObject.ObjectTypes.CameraRig) {
+						grabberRigidbody = playerObjectScript.GetComponent<Rigidbody>();
+					}
+				}
+			}
+		}
+
+		isUsed = true;
+	}
+
+	private void OnUnuse(object sender, InteractableObjectEventArgs e) {
+		isUsed = false;
+		grabberRigidbody = null;
+	}
 
 }
