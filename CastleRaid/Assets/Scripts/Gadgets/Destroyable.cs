9ec46f95d251535cc18cdd39e4c8d820beb6e0ba
Mikko Juutinen <luzer59@hotmail.com>
2017-11-15 16:35:05 +0900

    Fixed wrong UVs and other things on M-Launcher, birds now explode in a loot burst of diamonds and set some static objects not so static.

--- CastleRaid/Assets/Scripts/Gadgets/Destroyable.cs
+++ CastleRaid/Assets/Scripts/Gadgets/Destroyable.cs
@@ -7,6 +7,12 @@ public class Destroyable : MonoBehaviour
     [Tooltip("Objects that are the this object's children but will be reparented when the parent would be destroyed")]
     [SerializeField]
     private GameObject[] savedObjects;
+    [SerializeField]
+    private GameObject[] spawnedObjects;
+    [SerializeField]
+    private float objectSpawnRadius;
+    [SerializeField]
+    private string[] effects;
 
     public void Destroy()
     {
@@ -14,6 +20,14 @@ public class Destroyable : MonoBehaviour
         {
             obj.transform.SetParent(transform.parent, true);
         }
+        foreach (GameObject obj in savedObjects)
+        {
+            Instantiate(obj, transform.position + Random.insideUnitSphere * objectSpawnRadius, Quaternion.identity);
+        }
+        foreach (string eff in effects)
+        {
+            GameManager.effectManager.GetEffect(eff, true, true, transform.position, transform.rotation);
+        }       
         Destroy(gameObject);
     }
 }
